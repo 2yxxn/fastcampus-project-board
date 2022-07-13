03381f8724688360cbc17677a763e3b41c442265
2yxxn <yunji383614@gmail.com>
2022-07-13 15:45:16 +0900

    #10 - 도메인 코드를 엔티티로 변환
    1 도메인 클래스 == 1 테이블 매핑되도록 설계
    생성자와 `equals()`, `hashcode()` 등이 도메인 맥락에 맞게끔 디테일하게 설정

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -1,18 +1,90 @@
 package com.fastcampus.projectboard.domain;
 
-import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "modifiedAt"),
+})
+@EntityListeners(AuditingEntityListener.class)
+@Entity
 public class Article {
     // 게시글
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter
+    @Column(nullable = false)
     private String title;                   // 제목
+    @Setter
+    @Column(nullable = false, length = 10000)
     private String content;                 // 본문
+    @Setter
+    // @Column 은 아무 옵션이 없을 때, 생략 가능
     private String hashtag;                 // 해시태그
 
+    // 양방향
+    @ToString.Exclude // 순환참조 방지
+    @OrderBy("id")
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
+    private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
+
+    @CreatedDate
+    @Column(nullable = false)
     private LocalDateTime createdAt;        // 생성일시
+    @CreatedBy
+    @Column(nullable = false, length = 100)
     private String createdBy;               // 생성자
+    @LastModifiedDate
+    @Column(nullable = false)
     private LocalDateTime modifiedAt;       // 수정일시
+    @LastModifiedBy
+    @Column(nullable = false, length = 100)
     private String modifiedBy;              // 수정자
+
+    // 생성자
+    protected Article() {}
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag) {
+        return new Article(title, content, hashtag);
+    }
+
+    // Id (PK) 에 대한 equals and hashcode
+    // Id 는 notnull
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && id.equals(article.id); // 영속화 되지 않은 entity 는 모두 다른 값으로 보겠다
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
