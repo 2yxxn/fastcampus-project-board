f5b48e86ae515f34f35c0d51219f153efced991d
2yxxn <yunji383614@gmail.com>
2022-09-06 10:00:59 +0900

    #37 - 회원 계정 도메인 변경사항을 영향받는 코드에 적용
    `id` -> `userId` 로 id 를 변경하는 내용을 모든 자바코드에 반영함.

--- src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
+++ src/main/java/com/fastcampus/projectboard/domain/UserAccount.java
@@ -8,7 +8,6 @@ import java.util.Objects;
 @Getter
 @ToString
 @Table(indexes = {
-        @Index(columnList = "userId", unique = true),
         @Index(columnList = "email", unique = true),
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy")
@@ -16,11 +15,9 @@ import java.util.Objects;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class UserAccount extends AuditingFields {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Id @Column(length = 50)
+    private String userId;
 
-    @Setter @Column(nullable = false, length = 50) private String userId;
     @Setter @Column(nullable = false) private String userPassword;
 
     @Setter @Column(length = 100) private String email;
@@ -43,11 +40,11 @@ public class UserAccount extends AuditingFields {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof UserAccount userAccount)) return false;
-        return id != null && id.equals(userAccount.id);
+        return userId != null && userId.equals(userAccount.userId);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(userId);
     }
 }
